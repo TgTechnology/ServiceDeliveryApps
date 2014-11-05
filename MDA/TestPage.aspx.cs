c467b01d9084a07f99d57d3b9b18703ed8e6ad35
TgTechnology <cthornton@tangerineglobal.com>
2014-11-05 11:25:49 -0800

    Added test code for connecting to remote iis instance

--- MDA/TestPage.aspx.cs
+++ MDA/TestPage.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Management;
+using Microsoft.Web.Administration;
+
 
 namespace MDA
 {
@@ -11,8 +14,26 @@ namespace MDA
     {
         protected override void OnLoad(EventArgs e)
         {
+            DeploymentInfo HotelInfo = new DeploymentInfo();
+            ServerManager SiteInfo = HotelInfo.GetSiteInfo("10.200.1.72");
+
+            foreach (var site in SiteInfo.Sites)
+            {
+                Response.Write("Web-Site : " + site.Name);
+                foreach (Application app in site.Applications)
+                {
+                    if (app.VirtualDirectories.Count > 0)
+                    {
+                        foreach (VirtualDirectory vdir in app.VirtualDirectories)
+                        {
+                            Response.Write("  Virtual Directory: " + vdir.PhysicalPath + "<br>");
+                        }
+                    }
+                }
+            }
            
         }
+       
         protected void Page_Load(object sender, EventArgs e)
         {
 
